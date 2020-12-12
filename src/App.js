8fc076ea05cb23dc8dba83f4a6041160ef051275
abogonosov <a.bogonosov@pangeoradar.ru>
2020-12-12 17:51:38 +0300

    basic components and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React, { Component } from 'react'
 import './App.css';
+import CardList from './components/card-list/CardList'
+import SearchBox from './components/search-box/SearchBox'
 
-function App() {
-  return (
-    <div className="App">
-      <p>Roledex</p>
-    </div>
-  );
+class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(r => r.json())
+      .then(r => this.setState({monsters: r}))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+  render () {
+    const {monsters, searchField} = this.state
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className='App'>
+        <h1> Monsters Roledex </h1>
+        <SearchBox
+          placeholder='search monster!'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
